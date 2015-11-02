516927646a85d0339d1c605e148b143377d547ae
nath <gs.nathy@yahoo.es>
2015-11-02 16:05:46 -0600

    first

---
+++ app/src/BIES/db/Database.php
@@ -0,0 +1,11 @@
+<?php
+namespace RACCOON\BE\DB;
+
+class Database{
+public function __construct() {
+        ;
+    }
+    public static function getConection(){
+      /*   return new \PDO("mysql:host=localhost:3307;dbname=creditos", "root","usbw");*/
+    }
+}
